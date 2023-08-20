ff1579237bf82f3b0a1dec2e85d573c1a440c157
Zyrus Lee Inso <zyrusinso@gmail.com>
2023-08-20 21:21:53 +0800

    Initial Commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('first_name', 25);
+            $table->string('last_name', 25);
+            $table->string('email', 50)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->nullable();
+            $table->boolean('owner')->default(false);
+            $table->string('photo_path', 100)->nullable();
+            $table->rememberToken();
+
+            $table->foreignId('account_id')->index()->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
